bec8d66754645fbb0d77bbcf2dcb1757c553e550
Olena457 <olenademianenko42@gmail.com>
2024-05-12 21:34:25 +0300

    created task1

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,16 @@
 import Profile from './components/Profile/Profile';
 import userData from './data.json';
-// import userData from './components/userData';
-
-console.log(userData);
-// const userData = {
-//   username: 'Jacques Gluke',
-//   tag: 'jgluke',
-//   location: 'Ocho Rios, Jamaica',
-//   avatar: 'https://cdn-icons-png.flaticon.com/512/2922/2922506.png',
-//   stats: {
-//     followers: 5603,
-//     views: 4827,
-//     likes: 1308,
-//   },
-// };
 
 function App() {
   return (
     <>
-      App
-      <Profile />
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
     </>
   );
 }
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,4 +1,45 @@
-const Profile = () => {
-  return <h2>Profile</h2>;
+import css from './Profile.module.css';
+
+const Profile = ({
+  name,
+  tag,
+  location,
+  avatar,
+  stats: { followers, views, likes },
+}) => {
+  return (
+    <div className={css.containner}>
+      <div className={css.card}>
+        <img className={css.imageSvg} src={avatar} alt={name} />
+        <p className={css.nameHero}>{name}</p>
+        <p className={css.email}>{tag}</p>
+        <p className={css.location}>{location}</p>
+      </div>
+      <ul className={css.wraper}>
+        <li className={css.itemList}>
+          <span
+            className={css.spanText}
+            style={{ borderRight: '3px solid #474f54' }}
+          >
+            {followers}
+          </span>
+          <span className={css.number}>1000</span>
+        </li>
+        <li className={css.itemList}>
+          <span
+            className={css.spanText}
+            style={{ borderRight: '3px solid #474f54' }}
+          >
+            {views}
+          </span>
+          <span className={css.number}>2000</span>
+        </li>
+        <li className={css.itemList}>
+          <span className={css.spanText}>{likes}</span>
+          <span className={css.number}>3000</span>
+        </li>
+      </ul>
+    </div>
+  );
 };
 export default Profile;
