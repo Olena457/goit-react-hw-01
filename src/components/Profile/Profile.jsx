0fa6076af62332cc4dc5a8cec36298a31ed00391
Olena457 <olenademianenko42@gmail.com>
2024-05-13 01:01:16 +0300

    corected tack2

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,5 +1,4 @@
 import css from './Profile.module.css';
-
 const Profile = ({
   name,
   tag,
@@ -10,33 +9,23 @@ const Profile = ({
   return (
     <div className={css.containner}>
       <div className={css.card}>
-        <img className={css.imageSvg} src={avatar} alt={name} />
+        <img className={css.imageSvg} src={avatar} alt="" />
         <p className={css.nameHero}>{name}</p>
-        <p className={css.email}>{tag}</p>
+        <p className={css.tag}>jgluke</p>
         <p className={css.location}>{location}</p>
       </div>
       <ul className={css.wraper}>
         <li className={css.itemList}>
-          <span
-            className={css.spanText}
-            style={{ borderRight: '3px solid #474f54' }}
-          >
-            {followers}
-          </span>
-          <span className={css.number}>1000</span>
+          <span className={css.spanText}>Followers</span>
+          <span className={css.number}>{followers}</span>
         </li>
         <li className={css.itemList}>
-          <span
-            className={css.spanText}
-            style={{ borderRight: '3px solid #474f54' }}
-          >
-            {views}
-          </span>
-          <span className={css.number}>2000</span>
+          <span className={css.spanText}>Views</span>
+          <span className={css.number}>{views}</span>
         </li>
         <li className={css.itemList}>
-          <span className={css.spanText}>{likes}</span>
-          <span className={css.number}>3000</span>
+          <span className={css.spanText}>Likes</span>
+          <span className={css.number}>{likes}</span>
         </li>
       </ul>
     </div>
