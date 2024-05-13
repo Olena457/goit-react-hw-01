d3bd54491996f4b56241276f485a57e305ba907c
Olena457 <olenademianenko42@gmail.com>
2024-05-13 21:12:26 +0300

    created home-work1

--- src/components/Transaction/Transaction.jsx
+++ src/components/Transaction/Transaction.jsx
@@ -1,23 +1,13 @@
 import css from './Transaction.module.css';
+
 const OneTransaction = ({ type, amount, currency }) => {
   return (
-    <table className={css.table}>
-      <thead className={css.head}>
-        <tr>
-          <th className={css.th}>{type}</th>
-          <th className={css.th}>{type}</th>
-          <th className={css.th}>{type}</th>
-        </tr>
-      </thead>
-
-      <tbody className={css.tbody}>
-        <tr>
-          <td className={css.td}>{type}</td>
-          <td className={css.td}>{amount}</td>
-          <td className={css.td}>{currency}</td>
-        </tr>
-      </tbody>
-    </table>
+    <tr className={css.tr}>
+      <td className={css.td}>{type}</td>
+      <td className={css.td}>{amount}</td>
+      <td className={css.td}>{currency}</td>
+    </tr>
   );
 };
+
 export default OneTransaction;
--- src/components/Transactiontable/Transactiontable.jsx
+++ src/components/Transactiontable/Transactiontable.jsx
@@ -1,20 +1,28 @@
-import clsx from 'clsx';
 import css from './Transactontable.module.css';
 import OneTransaction from '../Transaction/Transaction.jsx';
+
 const TransactionHistory = ({ transactions }) => {
   return (
-    <div className={clsx(css.container)}>
-      {transactions.map(transaction => {
-        return (
+    <table className={css.table}>
+      <thead className={css.head}>
+        <tr>
+          <th className={css.th}>Type</th>
+          <th className={css.th}>Amount</th>
+          <th className={css.th}>Currency</th>
+        </tr>
+      </thead>
+      <tbody className={css.tbody}>
+        {transactions.map(transaction => (
           <OneTransaction
             key={transaction.id}
             type={transaction.type}
             amount={transaction.amount}
             currency={transaction.currency}
           />
-        );
-      })}
-    </div>
+        ))}
+      </tbody>
+    </table>
   );
 };
+
 export default TransactionHistory;
